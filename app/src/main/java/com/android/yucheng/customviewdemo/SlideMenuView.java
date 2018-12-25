8ce4df9b33a643fce6e908a59f60925512bdea7b
lingjiu <502241688@qq.com>
2018-12-25 10:48:07 +0800

    侧边栏view增加滑动速率判断

--- app/src/main/java/com/android/yucheng/customviewdemo/SlideMenuView.java
+++ app/src/main/java/com/android/yucheng/customviewdemo/SlideMenuView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
@@ -19,6 +20,13 @@ import android.widget.Scroller;
  * scroller.startScroll();(开始滚动)
  * computeScroll;//重绘回调(draw方法里面可以看到)
  * scroller.computeScrollOffset()(是否结束滚动)
+ * VelocityTracker用法:(用于追踪触摸事件的速率,实现flinging效果)
+ * 1mVelocityTracker = VelocityTracker.obtain()初始化
+ * 2mVelocityTracker.addMovement(event)追踪event事件
+ * 3mVelocityTracker.computeCurrentVelocity(1000);计算速率
+ * mVelocityTracker.getXVelocity()x方向上的速率
+ * 4mVelocityTracker.recycle()回收资源
+ * 补充:视差效果
  */
 public class SlideMenuView extends ViewGroup {
     private final String TAG = getClass().getSimpleName();
@@ -30,6 +38,9 @@ public class SlideMenuView extends ViewGroup {
     //是否是滑动状态
     private boolean isTouchSlop;
     private Scroller scroller;
+    private VelocityTracker mVelocityTracker;
+    //滚动显示和隐藏左侧布局时，手指滑动需要达到的速度。
+    public static final int SNAP_VELOCITY = 200;
 
     public SlideMenuView(Context context) {
         super(context);
@@ -80,18 +91,12 @@ public class SlideMenuView extends ViewGroup {
 
     private float downX, downY;
 
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        return super.dispatchTouchEvent(ev);
-    }
-
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
+        createVelocityTracker(ev);
         if (MotionEvent.ACTION_DOWN == ev.getAction()) {
             downX = ev.getX();
             downY = ev.getY();
-
         } else if (MotionEvent.ACTION_MOVE == ev.getAction()) {
 
             float offsetX = downX - ev.getX();
@@ -108,27 +113,49 @@ public class SlideMenuView extends ViewGroup {
                     scrollTo((int) -mMenuViewWidth, 0);
                 } else {
                     scrollBy((int) offsetX, 0);
+                    mainView.scrollBy((int) (-offsetX * 0.8f), 0);
                 }
                 downX = ev.getX();
                 downY = ev.getY();
             }
         } else if (MotionEvent.ACTION_UP == ev.getAction()) {
-            Log.i(TAG, " getScrollX() = " + getScrollX());
-            if (getScrollX() < 0 && getScrollX() > -mMenuViewWidth / 2) {
-                scroller.startScroll(getScrollX(), 0, -getScrollX(), 0);
-            } else if (getScrollX() > -mMenuViewWidth && getScrollX() < -mMenuViewWidth / 2) {
-                scroller.startScroll(getScrollX(), 0, (int) (-mMenuViewWidth - getScrollX()), 0);
+            if (isTouchSlop) {
+                mVelocityTracker.computeCurrentVelocity(1000);
+                //x方向上的速率
+                float xVelocity = mVelocityTracker.getXVelocity();
+                Log.i(TAG, " xVelocity = " + xVelocity + "   " + (xVelocity > SNAP_VELOCITY));
+                if ((getScrollX() < 0 && getScrollX() > -mMenuViewWidth / 2 && xVelocity < SNAP_VELOCITY) || xVelocity < -SNAP_VELOCITY) {
+                    Log.i(TAG, " getScrollX() = 1" + getScrollX());
+                    scroller.startScroll(getScrollX(), 0, 0 - getScrollX(), 0, 400);
+                } else if ((getScrollX() > -mMenuViewWidth && getScrollX() < -mMenuViewWidth / 2) || xVelocity > SNAP_VELOCITY) {
+                    Log.i(TAG, " getScrollX() = 2" + getScrollX());
+                    scroller.startScroll(getScrollX(), 0, (int) (-mMenuViewWidth - getScrollX()), 0, 400);
+                }
+                postInvalidate();
+                isTouchSlop = false;
+                recyclerVelocityTracker();
             }
-            postInvalidate();
-            isTouchSlop = false;
         }
         return true;
+    }
+
+    private void recyclerVelocityTracker() {
+        mVelocityTracker.recycle();
+        mVelocityTracker = null;
+    }
+
+    private void createVelocityTracker(MotionEvent event) {
+        if (mVelocityTracker == null) {
+            mVelocityTracker = VelocityTracker.obtain();
+        }
+        mVelocityTracker.addMovement(event);
     }
 
     @Override
     public void computeScroll() {
         if (scroller.computeScrollOffset()) {
             scrollTo(scroller.getCurrX(), 0);
+            mainView.scrollTo(-(int) (scroller.getCurrX() * 0.8f), 0);
             postInvalidate();
         }
     }
